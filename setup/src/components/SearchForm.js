b5dec13e9390adf9e079eb460e3b31fcd8138386
upneet2213 <upneet2213@gmail.com>
2021-08-02 21:47:24 +0530

    Initial commit

---
+++ setup/src/components/SearchForm.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useGlobalContext } from '../context'
+
+const SearchForm = () => {
+  const {setSearchterm}= useGlobalContext();
+  const searchValue = React.useRef('');
+  React.useEffect(()=>{
+    searchValue.current.focus()
+  },[])
+  const searchCocktail=()=>{
+    setSearchterm(searchValue.current.value)
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+  }
+  return (
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">Search your favorite cocktail</label>
+          <input type="text" id="name" ref={searchValue} onChange={searchCocktail}/>
+        </div>
+      </form>
+    </section>
+  )
+}
+
+export default SearchForm
---
+++ setup/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nihil ipsa non beatae architecto vero saepe a sint! Sed illo dicta corrupti tenetur accusantium suscipit eius consectetur est incidunt animi voluptatem debitis distinctio modi, harum nostrum iste placeat, molestiae itaque.</p>
+    </section>
+  )
+}
+
+export default About
